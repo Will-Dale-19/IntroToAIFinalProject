5ff5c057fb6a79c3fb9d8b62a8423140d3182f2f
Ethan <schillee@msoe.edu>
2023-05-10 13:17:52 -0500

    fixed Blackjack

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,28 @@ public class Main {
     public static void main(String[] args) {
         boolean continuePlaying = true;
         while (continuePlaying) {
-            Blackjack game = new Blackjack(new Player("Player"));
+            System.out.println("How many players would you like? (1-3)");
+            Scanner in = new Scanner(System.in);
+            int numPlayers = Integer.parseInt(in.nextLine());
+            Blackjack game;
+            switch (numPlayers) {
+                case 1:
+                    game = new Blackjack(new Player("Player 1"));
+                    break;
+                case 2:
+                    game = new Blackjack(new Player("Player 1"), new Player("Player 2"));
+                    break;
+                case 3:
+                    game = new Blackjack(new Player("Player 1"), new Player("Player 2"), new Player("Player 3"));
+                    break;
+                default:
+                    game = new Blackjack(new Player("Player"));
+            }
             game.startGame();
             boolean gamePlaying = true;
             while (gamePlaying) {
                 gamePlaying = game.round();
             }
-            Scanner in = new Scanner(System.in);
             boolean loop = true;
             while (loop) {
                 System.out.println("\nWould you like to play again?");
